ef5e6a85d0721d20d670ad957a7788d174627d24
nikkig <nikkig@nikkigs-MBP.home>
2023-07-11 11:10:56 -0700

    more set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function Home() {
+  const [userPrompt, setUserPrompt] = useState("")
+
+  const handleUserPrompt = (e) => {
+    setUserPrompt(e.target.value)
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>ChatGPT Clone</h1>
+      
+      <textarea placeholder="ask me anything" value={userPrompt} onChange={(e) => handleUserPrompt(e)} />
+      
     </main>
   )
 }
